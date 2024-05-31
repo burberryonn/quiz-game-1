545cb8db69822a4ccf537344c053ace86790219d
MashaShamash <mariapanasenko26@yandex.ru>
2024-05-31 12:13:46 +0300

    done

--- View/view.js
+++ View/view.js
@@ -1,16 +1,43 @@
-const inquirer = require('inquirer');
-inquirer
-  .prompt([
-    { type: 'input', name: 'username', message: 'Введи имя:' },
-    {
-      type: 'list',
-      name: 'bonuses',
-      message: 'На сколько ты опоздал?',
-      choices: [
-        { name: 'Не опаздывал', value: 0 },
-        { name: 'На 2-3 минутки', value: 2 },
-        { name: 'Опоздал и не сообщил', value: 5 },
-      ],
-    },
-  ])
-  .then((answers) => console.log(answers));
+const inquirer = require("inquirer");
+
+class View {
+  async startQuiz() {
+    return inquirer.prompt([
+      {
+        type: "list",
+        name: "category",
+        message: "Выберите категорию",
+        choices: ["Еда", "Эльбрус", "Смешное"],
+      },
+    ]);
+  }
+  async pullQuestions({ question, answer, choices }, counter) {
+    let userAnswer;
+    if (choices.length > 1) {
+      userAnswer = await inquirer.prompt([
+        {
+          type: "list",
+          name: "userAnswer",
+          message: question,
+          choices,
+        },
+      ]);
+    } else {
+      userAnswer = await inquirer.prompt([
+        {
+          type: "input",
+          name: "userAnswer",
+          message: question,
+        },
+      ]);
+    }
+    if (userAnswer.userAnswer.trim().toLowerCase() === answer.toLowerCase()) {
+      console.log("Умничко!! :purple_heart:");
+      counter += 100;
+    } else {
+      console.log("Не совсем так :sob:");
+    }
+    return counter;
+  }
+}
+module.exports = View;
